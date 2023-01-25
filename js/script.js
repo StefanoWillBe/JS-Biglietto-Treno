7b5e64b4abe0eb6309d75ca16cad3b4de01a5fb8
Stefano Russo <consigliererusso@gmail.com>
2023-01-25 18:11:25 +0100

    Modificato

--- js/script.js
+++ js/script.js
@@ -1,73 +1,73 @@
 // salvo in una variabile il bottone genera
-var generateButton = document.getElementById("generate");
+const generateButton = document.getElementById( "generate" );
 
 // rimango in ascolto di un evento click sul bottone genera
-generateButton.addEventListener("click" ,
-    function (){
-
+generateButton.addEventListener("click" , function ()
+{
     // leggo i valori di nome e cognome, km inseriti ed età inseriti dall'utente
-    var nomeUser = document.getElementById("nome-cognome").value;
-    var kmInserti = document.getElementById("km-inseriti").value;
-    var kmInsert = parseInt(kmInserti);
-    var etaInser = document.getElementById("eta-inserita").value;
+    const userName = document.getElementById( "nome-cognome" ).value;
+    
+    const kmInseriti = parseInt(document.getElementById( "km-inseriti" ).value);
+
+    const etaInserita = document.getElementById( "eta-inserita" ).value;
+
+    if( !userName || !kmInseriti || !etaInserita ) {
+        
+        alert(" Attenzione: tutti i campi devono essere compilati");
+
+    }else{
+
+        // assegno in una variabile il costo al km e prezzo standard
+        const costokm = 0.21;
+        let finalPrice = costokm * kmInseriti;
+
+     // calcolo offerta: sconto 20% se minorenne, 40% se over 65 o costo standard se maggiorenne
+        if( etaInserita == "minorenne" ){
+            finalPrice -= ( finalPrice * 20 / 100 );
+        } else if ( etaInserita == "over" ){
+            finalPrice -= ( finalPrice * 40 / 100 );
+        }
+
+        // STAMPARE IL BIGLIETTO
+        document.getElementById( "name-user" ).innerHTML = userName;
+        document.getElementById( "offer-user" ).innerHTML = etaInserita;
+        document.getElementById( "carriage-user" ).innerHTML = randomCarrozza();
+        document.getElementById( "code-user" ).innerHTML = randomTrain();
+        document.getElementById( "cost-user" ).innerHTML = finalPrice.toFixed(2);
+    
+        // al click del bottone genera visualizzo il biglietto che altrimenti ha display none
+        document.getElementById( "ticket-user" ).className = "block";
+
+    }    
+});
 
 
-    // assegno in una variabile il costo al km e prezzo standard
-    var costokm = 0.21;
-    var finalPrice = costokm * kmInsert;
-
-// calcolo offerta: sconto se minorenne o overe se maggiorenne costo standard
-    if(etaInser == "minorenne"){
-        finalPrice -= (finalPrice * 20 / 100);
-    } else if (etaInser == "over"){
-        finalPrice -= (finalPrice * 20 / 100);
-    }
-
-    // STAMPARE IL BIGLIETTO
-    document.getElementById("name-user").innerHTML = nomeUser;
-    document.getElementById("offer-user").innerHTML = etaInser;
-    document.getElementById("carriage-user").innerHTML = getRandomcarrozza();
-    document.getElementById("code-user").innerHTML = getRandomTrain();
-    document.getElementById("cost-user").innerHTML = finalPrice.toFixed(2);
-
-    // al click del bottone genera visualizzo il biglietto che altrimenti ha display none
-    document.getElementById("ticket-user").className = "block";
-})
-
-// Funzione per generare codice random per carrozza treno da 1 a 10
-function getRandomcarrozza(min) {
-    return Math.floor(Math.random() * 10) + 1;
+// Funzione per generare codice random per carrozza treno 
+function randomCarrozza(min) {
+    return Math.floor(Math.random() * 20) + 1;
 }
 
-// Funzione per generare codice random per CP treno da 1000 a 10000
-function getRandomTrain(min) {
+// Funzione per generare codice random per CP treno 
+function randomTrain(min) {
     return Math.floor(Math.random() * 10000) + 1;
 }
 
 
-// salvo in una variabile il bottone genera
-var cancelButton = document.getElementById("cancel");
+// salvo in una variabile il bottone cancel
+const cancelButton = document.getElementById( "cancel" );
 
 // rimango in ascolto di un evento click sul bottone annulla
 cancelButton.addEventListener("click" ,
     function (){
 
     // Tolgo il Biglietto
-    document.getElementById("ticket-user").className = "none";
+    document.getElementById( "ticket-user" ).className = "none";
 
     // svuoto gli input
-    var nomeUser = document.getElementById("nome-cognome").value = "";
-    var kmInserti = document.getElementById("km-inseriti").value = "";
-    var kmInsert = parseInt(kmInserti);
-    var etaInser = document.getElementById("eta-inserita").value = "";
+    const userName = document.getElementById( "nome-cognome" ).value = "";
+    const kmInseriti = parseInt(document.getElementById( "km-inseriti" ).value ="");
+    const etaInserita = document.getElementById( "eta-inserita" ).value = "";
 
-    // svuoto gli altri elementi
-    document.getElementById("name-user").innerHTML = "";
-    document.getElementById("offer-user").innerHTML = "";
-    document.getElementById("carriage-user").innerHTML = "";
-    document.getElementById("code-user").innerHTML = "";
-    document.getElementById("cost-user").innerHTML = "";
-
-})
+});
 
 
