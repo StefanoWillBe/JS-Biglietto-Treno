78b751a2202d2d73275a95feec42456c55313a9a
Stefano Russo <consigliererusso@gmail.com>
2023-01-25 21:11:51 +0100

    Final

--- js/script.js
+++ js/script.js
@@ -7,11 +7,12 @@ generateButton.addEventListener("click" , function ()
     // leggo i valori di nome e cognome, km inseriti ed età inseriti dall'utente
     const userName = document.getElementById( "nome-cognome" ).value;
     
+    
     const kmInseriti = parseInt(document.getElementById( "km-inseriti" ).value);
 
     const etaInserita = document.getElementById( "eta-inserita" ).value;
 
-    if( !userName || !kmInseriti || !etaInserita ) {
+    if( (userName.length == 0  ) || (kmInseriti.length == 0 || !kmInseriti ) || (etaInserita.length == 0 )) {
         
         alert(" Attenzione: tutti i campi devono essere compilati");
 
@@ -19,27 +20,39 @@ generateButton.addEventListener("click" , function ()
 
         // assegno in una variabile il costo al km e prezzo standard
         const costokm = 0.21;
-        let finalPrice = costokm * kmInseriti;
+        let prezzoFinale = costokm * kmInseriti;
 
      // calcolo offerta: sconto 20% se minorenne, 40% se over 65 o costo standard se maggiorenne
         if( etaInserita == "minorenne" ){
-            finalPrice -= ( finalPrice * 20 / 100 );
+            prezzoFinale -= ( prezzoFinale * 20 / 100 );
         } else if ( etaInserita == "over" ){
-            finalPrice -= ( finalPrice * 40 / 100 );
+            prezzoFinale -= ( prezzoFinale * 40 / 100 );
         }
 
-        // STAMPARE IL BIGLIETTO
-        document.getElementById( "name-user" ).innerHTML = userName;
-        document.getElementById( "offer-user" ).innerHTML = etaInserita;
-        document.getElementById( "carriage-user" ).innerHTML = randomCarrozza();
-        document.getElementById( "code-user" ).innerHTML = randomTrain();
-        document.getElementById( "cost-user" ).innerHTML = finalPrice.toFixed(2);
+        stampaBiglietto(userName, etaInserita, prezzoFinale);
     
         // al click del bottone genera visualizzo il biglietto che altrimenti ha display none
         document.getElementById( "ticket-user" ).className = "block";
 
     }    
 });
+
+
+/**
+ * Funzione che stampa il biglietto
+ * @param {string} user Nome della persona che compra il biglietto
+ * @param {int} eta Età della persona che compra il biglietto
+ * @param {float} prezzo Prezzo totale del biglietto
+ */
+function stampaBiglietto(user, eta, prezzo) {
+
+      // STAMPARE IL BIGLIETTO
+      document.getElementById( "name-user" ).innerHTML = user;
+      document.getElementById( "offer-user" ).innerHTML = eta;
+      document.getElementById( "carriage-user" ).innerHTML = randomCarrozza();
+      document.getElementById( "code-user" ).innerHTML = randomTrain();
+      document.getElementById( "cost-user" ).innerHTML = prezzo.toFixed(2);
+}
 
 
 // Funzione per generare codice random per carrozza treno 
